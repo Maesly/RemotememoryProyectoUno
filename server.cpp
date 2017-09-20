d32f29de33687bed861983b6793ed3d4aeb88cb2
Maesly <nadieska23@outlook.es>
2017-09-20 03:34:09 -0600

    Actualizaciones del servidor

--- server.cpp
+++ server.cpp
@@ -18,6 +18,7 @@
 #include <thread>
 #include <stdio.h>
 #include "server.h"
+#include "lista.h"
 
 using namespace std;
 /**
@@ -71,52 +72,42 @@ void Servidor::correr(){
 
     }
 
-    cout <<"Bienvenido, la conexion ha sido creada..."<< endl;
+    cout <<"Bienvenido"<< endl;
     size = sizeof(server_addr);
     listen(cliente,1); // El servidor escucha a los clientes
 
     while(!salir) {                                                             // condicion que mantiene corriendo el servidor
-        servidor = accept(cliente, (struct sockaddr *) &server_addr, &size);    ///Acepta a los clientes que deseen conectarse
-        strcpy(buffer, "Servidor conectado...\n");                              //Se setea el mensaje a enviar al cliente
-        send(servidor, buffer, bufsize, 0);                                     /// Envía mensaje de respuesta al cliente cuando este se conecta
-        cout << "Conexion exitosa!" << endl;
-        cout << "Poner * al final del mensaje* \nEscriba # para finalizar la conexion..." << endl;
-        cout << "Mensaje recibido: \n";
-    do {
-        recv(servidor, buffer, bufsize, 0);  //recibe la respuesta del cliente
-        cout << buffer << " ";
-        if (*buffer == '#') {
-            *buffer = '*';
-            salir = true;
-        }
-    } while (*buffer != '*');
+        servidor = accept(cliente,(struct sockaddr*) &server_addr,&size);
+        if(servidor>=0){
+            char* buffer = (char*) calloc(1,1024);
+            while(!salir){
+                recv(servidor, buffer, 1024, 0);
+                printf("%s\n",buffer);
+                if(*buffer == '1'){
+                    Tlista* lista = new Tlista;
+                    insertarInicio(reinterpret_cast<Tlista &>(lista), 45);
+                    reportarLista(reinterpret_cast<Tlista>(lista));
+                    send(servidor,buffer,1024,0);
+                }
+                else if(*buffer == '2') {
+                    cout << "hla";
+                }else if(*buffer == '3'){
+                        cout <<"#jojo";
+                }else{
+                    close(cliente);
+                }
 
-        do{
-            cout << "Escriba mensaje: ";
-            do{
-                cin>>buffer;
-                send(servidor,buffer,bufsize,0);
-                if(*buffer == '#'){
-                    send(servidor,buffer,bufsize,0);
-                    *buffer = '*';
-                    salir = true;
-                }
-            }while(*buffer !='*');
-            cout<<"Mensaje recibido: \n";
-            do{
-                recv(servidor,buffer,bufsize,0);
-                cout<< buffer<<" ";
-                if(*buffer == '#'){
-                    *buffer = '*';
-                    salir = true;
-                }
-            }while (*buffer != '*');
-        }while(!salir);
-        cout << "\nTerminó conexion con "<<inet_ntoa(server_addr.sin_addr);
-        close(servidor);
-        cout<<"\nConecte nuevo cliente";
-        salir = false;
+
+                //exit(0);
+            }
+            close(cliente);
+        }else {
+            close(cliente);
+            //break;
+            //cout << "Error aceptando el cliente" << endl;
+            //exit(1);
+
+
+        }
     }
-    close(cliente);
-   // return ;
 }
